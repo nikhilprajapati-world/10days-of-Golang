c8002e9f3a895417c9c04a38ccb7fe111a9d2ccb
nikhilprajapati-world <nikhil.prajapati@live.in>
2024-03-05 19:09:30 +0530

    if else

---
+++ day04/ifelse.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age int = 5
+
+	if age > 21 {
+		fmt.Println("You are under age with: ", age)
+	} else {
+		fmt.Println("Welcome to the club !!!")
+	}
+
+}
